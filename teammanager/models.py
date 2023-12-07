9aef1278d17069d5d42a8722577ed97229f1f1be
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2023-12-07 14:11:13 -0800

    update class-base view and change html structures

--- teammanager/models.py
+++ teammanager/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+ROLE_OPTION = [
+  ("admin", "Admin"),
+  ("regular", "Regular")
+]
 
 class Member(models.Model):
   first_name = models.CharField(max_length=200)
   last_name = models.CharField(max_length=200)
   email = models.CharField(max_length=200)
   location = models.CharField(max_length=200)
-  is_admin = models.BooleanField(default=False)
+  role = models.CharField(max_length=8, choices=ROLE_OPTION, default="regular")
   def __str__(self):
-    return self.first_name + " " + self.last_name + " " + str(self.is_admin)
+    return self.first_name + " " + self.last_name + " "
   def isAdmin(self):
-    return self.is_admin
+    return self.role == "admin"
+  
+class MemberForm(ModelForm):
+  class Meta:
+      model = Member
+      fields = "__all__"
--- teammanager/views.py
+++ teammanager/views.py
@@ -1,76 +1,47 @@
-from django.http import Http404, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
-from .models import Member
+from .models import Member, MemberForm
+from django.shortcuts import get_object_or_404
 
-class MemberView(generic.ListView):
-    template_name = "teammanager/index.html"
-    context_object_name = "existing_members"
+class MemberView(ListView):
+	context_object_name = "existing_members"
+	queryset = Member.objects.order_by("id")
+	template_name = "teammanager/index.html"
 
-    def get_queryset(self):
-        return Member.objects.order_by("id")
+class EditView(UpdateView):
+	model = Member
+	form_model = MemberForm
+	fields = '__all__'
+	template_name = "teammanager/edit.html"
+	success_url = ("/teammanager")
+	
+	def get_object(self, queryset=None):
+		return get_object_or_404(Member, pk=self.kwargs['pk'])
 
-class EditView(generic.DetailView):
-    model = Member
-    template_name = "teammanager/edit.html"
+class AddView(FormView):
+	model = Member
+	template_name = "teammanager/add.html"
+	form_model = MemberForm
+    
+	def get(self, request):
+		form = self.form_model()
+		return render(request, self.template_name, {'form': form})
+	
+	def post(self, request):
+		form = MemberForm(request.POST)
+		if form.is_valid():
+			form.save()
+		else:
+			return render(
+				request,
+				"teammanager/add.html", {
+					"error_message": "Please input all the required field."
+				}
+			)
+		return HttpResponseRedirect("/teammanager")
 
-class AddView(CreateView):
-    model = Member
-    template_name = "teammanager/add.html"
-    fields = ["first_name"]
-
-def add_member(request):
-    try:
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        location = request.POST['location']
-        role = request.POST['role']
-        admin = False
-        if (role == "admin"):
-            admin = True
-        newMember = Member(first_name=first_name, last_name=last_name, email=email, location=location, is_admin=admin)
-        newMember.save()
-    except (KeyError, Member.DoesNotExist):
-        return render(
-            request,
-            "teammanager/add.html", {
-                "error_message": "Please input all the required field."
-            }
-        )
-    return HttpResponseRedirect("/teammanager")
-
-def edit_member(request, pk):
-    member = Member.objects.get(id=pk)
-    try:
-        member.first_name = request.POST['first_name']
-        member.last_name = request.POST['last_name']
-        member.email = request.POST['email']
-        member.location = request.POST['location']
-        role = request.POST['role']
-        member.is_admin = False
-        if (role == "admin"):
-            member.is_admin = True
-        member.save()
-    except (KeyError, Member.DoesNotExist):
-        return render(
-            request,
-            "teammanager/edit.html", {
-                "error_message": "Please input all the required field."
-            }
-        )
-    return HttpResponseRedirect("/teammanager")
-
-def delete_member(request, pk):
-    try:
-        member = Member.objects.get(id=pk)
-    except (KeyError, Member.DoesNotExist):
-        return render(
-            request,
-            "teammanager/index.html", {
-                "error_message": "The member does not exist."
-            }
-        )
-    member.delete()
-    return HttpResponseRedirect("/teammanager")
+class DeleteMember(DeleteView):
+	model = Member
+	success_url = ("/teammanager")
